a6e7d8cab7144e005fedd5bef54060bc32ad004b
Fikri Fri Yanto <fikrifyanto@gmail.com>
2022-02-11 08:08:45 +0700

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# fikrifyanto.github.io
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,42 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/*.html"],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    fontFamily: {
+      'inter': [ 'Inter', 'sans-serif']
+    },
+    extend: {
+      animation: {
+        blob: "blob 4s infinite",
+        color: "color 8s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+        color: {
+          "0%": {
+            backgroundColor: '#FFFFFF',
+          },
+          "50%": {
+            backgroundColor: '#f0f9ff',
+          },
+          "100%": {
+            backgroundColor: '#FFFFFF',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
